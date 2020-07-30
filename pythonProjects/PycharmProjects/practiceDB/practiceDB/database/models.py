0f94e9cfa9e94251b54220046fe772146052356e
piyush5kasturi <piyush5kasturi@gmail.com>
2020-07-30 19:49:32 +0530

    add database table

--- pythonProjects/PycharmProjects/practiceDB/practiceDB/database/models.py
+++ pythonProjects/PycharmProjects/practiceDB/practiceDB/database/models.py
@@ -4,5 +4,12 @@ from django.db import models
 
 class database(models.Model):
     student_id = models.AutoField
-    student_image = models.ImageField
+    student_image = models.ImageField(upload_to="database\images", default="")
+    student_name = models.TextField()
+    student_roll = models.CharField(max_length=50)
+    student_class = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.student_name
 
