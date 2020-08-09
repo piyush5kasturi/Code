e19539beeaa50633e705fec75d635b0035614a25
piyush5kasturi <piyush5kasturi@gmail.com>
2020-08-09 12:40:58 +0530

    multicards done

--- pythonProjects/PycharmProjects/practiceDB/practiceDB/database/views.py
+++ pythonProjects/PycharmProjects/practiceDB/practiceDB/database/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import database
 from .form import ImageForm
+from math import ceil
 from django.core.files.storage import FileSystemStorage
 # from .models import Post
 from django.http import HttpResponse
@@ -15,15 +16,17 @@ def index(request):
         student_class = request.POST.get('student_class')
         student_image = request.FILES['image']
         fs = FileSystemStorage()
-        #filename = fs.save(student_image.name, student_image)
-        #uploaded_file_url = fs.url(filename)
         print(student_image, student_name, student_class)
-
         student_name = request.POST.get('student_name')
         student_class = request.POST.get('student_class')
         Databaseee = database(student_name=student_name, student_class=student_class, student_image=student_image)
         Databaseee.save()
-    return render(request, 'database/index.html')
+    data=database.objects.all()
+    print(data)
+    n=len(data)
+    nslides=n//4+ceil((n/4)-(n//4))
+    params={'no_of_slides':nslides,'range': range(1,nslides) , 'database': data}
+    return render(request, 'database/index.html',params)
 
 def card(request):
     return render(request,'database/card.html')
